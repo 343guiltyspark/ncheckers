a4087b2d28760bab599c4c7a3e240579fa647cbe
guiltySpark <emaldona1776@gmail.com>
2020-08-24 21:51:19 -0700

    Added Next.js Router and asPath to [gid].js route

--- pages/[gid].js
+++ pages/[gid].js
@@ -1,5 +1,10 @@
 import Home from "./index";
+import { useRouter } from "next/router";
 
 export default function game() {
-  return <Home />;
+  const router = useRouter();
+  let sessionId = router.asPath;
+  sessionId = sessionId.replace("/", "");
+  console.log(sessionId);
+  return <Home sessionId={sessionId} />;
 }
