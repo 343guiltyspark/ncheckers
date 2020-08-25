fd7850c7f6673329fc31787cf2e0ad48db964800
guiltySpark <emaldona1776@gmail.com>
2020-08-24 21:50:36 -0700

    Added new event and setIO to socket.tsx

--- helpers/sockets.tsx
+++ helpers/sockets.tsx
@@ -1,12 +1,21 @@
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://localhost:8080";
 
-export const socketConnect = (data, setStandBy) => {
+export const socketConnect = (data, setStandBy, setIO) => {
   const io = socketIOClient(ENDPOINT);
+
+  console.log(io);
   io.on("connect", () => {
     console.log("Connected");
+    io.emit("setSession", data);
   });
   io.on("onJoin", (msg) => {
-    setStandBy("activePlayer center waiting");
+    console.log(msg);
+    setStandBy(msg);
   });
+  io.on("secondPlayerJoined", (msg) => {
+    console.log("secondPlayerJoined", msg);
+    setStandBy(msg);
+  });
+  setIO(io);
 };
